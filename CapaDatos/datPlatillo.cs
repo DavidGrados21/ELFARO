1af648d7a105d9c3ec96d9a97f7631a2c6f40006
LauraThalia <117493171+LauraThalia@users.noreply.github.com>
2024-06-23 15:56:15 -0500

    Update datPlatillo.cs
    Datos

--- CapaDatos/datPlatillo.cs
+++ CapaDatos/datPlatillo.cs
@@ -88,6 +88,89 @@ namespace CapaDatos
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
+        public Boolean InsertarPlatillo(entPlatillo PL)
+        {
+            SqlCommand cmd = null;
+            Boolean inserta = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spInsertaPlatillo", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@NombrePlatillo", PL.NombrePlatillo);
+                cmd.Parameters.AddWithValue("@Precio", PL.Precio);
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    inserta = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (cmd != null && cmd.Connection != null)
+                {
+                    cmd.Connection.Close();
+                }
+            }
+            return inserta;
+        }
+        public Boolean EliminarPlatillo(string nombrePlatillo)
+        {
+            SqlCommand cmd = null;
+            Boolean delete = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spEliminaPlatillo", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@NombrePlatillo", nombrePlatillo);
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    delete = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return delete;
+
+        }
+        
+        public Boolean EditarPlatillo(String n, int p)
+        {
+            SqlCommand cmd = null;
+            Boolean edita = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spEditaPlatillo", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@NombrePlatillo", n);
+                cmd.Parameters.AddWithValue("@Precio ", p);
+                
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    edita = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return edita;
+        }
 
 
     }
