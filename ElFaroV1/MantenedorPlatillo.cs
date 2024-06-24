c89ac247164c5a68640a827ed3b60a1b1eaba7e5
DavidGrados21 <cesitar.14744@gmail.com>
2024-06-23 21:01:31 -0500

    Update MantenedorPlatillo.cs

--- ElFaroV1/MantenedorPlatillo.cs
+++ ElFaroV1/MantenedorPlatillo.cs
@@ -66,40 +66,19 @@ namespace ElFaroV1
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
-                try
-                {
-                    // Verificar si hay al menos una fila seleccionada en el DataGridView
-                    if (dgPlatillo.SelectedRows.Count > 0)
-                    {
-                        // Obtener la fila seleccionada
-                        DataGridViewRow selectedRow = dgPlatillo.SelectedRows[0];
-
-                        // Obtener los valores de las celdas de la fila seleccionada
-                        string n = selectedRow.Cells["NombrePlatillo"].Value.ToString();
-                        int p = Convert.ToInt32(selectedRow.Cells["Precio"].Value);
-
-                    // Asignar los valores a los controles correspondientes
-                    logPlatillo.Instancia.EditarPlatillo(n,p);                        // También podrías asignar estos valores a tu entidad entPlatillo
-                        // para usarlos posteriormente si lo deseas.
-                        // entPlatillo c = new entPlatillo();
-                        // c.NombrePlatillo = nombrePlatillo;
-                        // c.Precio = precio;
-                    }
-                    else
-                    {
-                        MessageBox.Show("Debe seleccionar un platillo en la lista.");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.Message);
-                }
-
-                // Llamar a métodos para limpiar y actualizar la visualización
-                LimpiarVariables();
-                MostrarPlatillos();
-            
+            try
+            {
+                entPlatillo c = new entPlatillo();
+                c.NombrePlatillo = txtNPlatillo.Text.Trim();
+                c.Precio = decimal.Parse(txtPrecio.Text.Trim());
+                logPlatillo.Instancia.EditarPlatillo(c);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error.." + ex);
+            }
+            LimpiarVariables();
+            MostrarPlatillos();
         }
 
     }
