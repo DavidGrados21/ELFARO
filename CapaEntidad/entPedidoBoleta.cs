900f2e7346b8003d327d740535799c21ddcb25d2
DavidGrados21 <cesitar.14744@gmail.com>
2024-07-04 10:24:58 -0500

    COPIA ENTIDAD

---
+++ CapaEntidad/entPedidoBoleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entPedidoBoleta
+    {
+        public int Cant { get; set; }
+        public string Cod { get; set; }
+        public decimal Precio { get; set; }
+        public string Platillo { get; set; }
+    }
+}
